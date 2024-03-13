b9868e7bda0f8c20706717a20ea9f16c20ae2545
SupGilllll <supgilllll1177@gmail.com>
2024-03-13 12:07:39 +0900

    update

--- src/run_script/experiment_0724_part1.py
+++ src/run_script/experiment_0724_part1.py
@@ -21,14 +21,14 @@ os.chdir('/data/zhao/MONN/src')
 measures = ['KIKD']
 settings = ['new_protein']
 thresholds = [0.3, 0.4, 0.5]
-# out_path = "../results/0724/baseline_seed42"
+# out_path = "../results/0724/baseline"
 
 # for measure in measures:
 #     for setting in settings:
 #         for threshold in thresholds:
 #             os.system(f'python CPI_train.py {measure} {setting} {threshold} blosum62 &> {out_path}/{measure}_{setting}_{threshold}.log')
 
-out_path = "../results/0724/transformer_base_seed42"
+out_path = "../results/0724/transformer_base"
 for measure in measures:
     for setting in settings:
         for threshold in thresholds:
